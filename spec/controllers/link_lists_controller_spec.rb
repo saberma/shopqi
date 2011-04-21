cf424ff325830499fe0cfff5939aa8c12bd6fd6b
saberma <mahb45@gmail.com>
2011-04-21 18:45:19 +0800

    add link_list controller rspec test.

--- spec/controllers/link_lists_controller_spec.rb
+++ spec/controllers/link_lists_controller_spec.rb
@@ -1,5 +1,31 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe LinkListsController do
+
+  let(:link_list) { Factory(:link_list) }
+  
+  let(:link_list_with_links) { Factory(:link_list_with_links) }
+
+  context '#update' do
+
+    it "without links" do
+      xhr :put, :update, :id => link_list.id, :link_list => link_list.attributes
+      response.should be_success
+    end
+
+    it "with links" do
+      attrs = link_list_with_links.attributes
+      attrs[:links] = []
+      link_list_with_links.links.each do |link|
+        attrs[:links] << link.attributes
+      end
+
+      ap attrs
+      xhr :put, :update, :id => link_list_with_links.id, :link_list => attrs
+      response.should be_success
+    end
+
+  end
 
 end
--- spec/factories/link_lists.rb
+++ spec/factories/link_lists.rb
@@ -7,6 +7,6 @@ Factory.define :link_list_with_links, :parent => :link_list do |f|
   f.title "热门品牌"
   f.links {[
     Factory.build(:link, :title => 'IBM'),
-    Factory.build(:link, :text => '华硕')
+    Factory.build(:link, :title => '华硕')
   ]}
 end
