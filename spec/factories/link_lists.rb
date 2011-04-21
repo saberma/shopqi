7d427183b775579032fc08c56a7597f8ebe352e3
saberma <mahb45@gmail.com>
2011-04-21 17:47:39 +0800

    add link and link list factory data.

--- spec/factories/link_lists.rb
+++ spec/factories/link_lists.rb
@@ -1,5 +1,12 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
+# encoding: utf-8
 Factory.define :link_list do |f|
-  f.title "MyString"
+  f.title "链接"
+end
+
+Factory.define :link_list_with_links, :parent => :link_list do |f|
+  f.title "热门品牌"
+  f.links {[
+    Factory.build(:link, :title => 'IBM'),
+    Factory.build(:link, :text => '华硕')
+  ]}
 end
