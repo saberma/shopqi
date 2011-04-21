ba8b4a127dabe5cffe7e5d6e9318cd8d8981cc1d
saberma <mahb45@gmail.com>
2011-04-21 21:37:47 +0800

    remove js file generated by coffeescript.

--- public/javascripts/models/link.js
+++
@@ -1,9 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Wed, 20 Apr 2011 14:44:03 GMT from
- * /vagrant/app/coffeescripts/models/link.coffee
- */
-
-var Link;
-Link = Backbone.Model.extend({
-  name: 'link',
-  url: '/admin/links'
-});
--- public/javascripts/models/link_list.js
+++
@@ -1,8 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Wed, 20 Apr 2011 04:16:38 GMT from
- * /vagrant/app/coffeescripts/models/link_list.coffee
- */
-
-var LinkList;
-LinkList = Backbone.Model.extend({
-  name: 'link_list'
-});
