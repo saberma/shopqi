c271ccd94fb090efcc13ddbc6c1b1ec7bbe18006
liwh <liwh87@gmail.com>
2011-04-26 21:29:45 +0800

    fixed: login page error and add rspec products spec test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe ProductsController do
   include Devise::TestHelpers
   
-  let(:iphone4) {Factory(:iphone4)}
+  let(:iphone4) {Factory.build(:iphone4)}
   before :each do 
     sign_in(Factory(:user_liwh))
   end
@@ -12,7 +12,7 @@ describe ProductsController do
   context '#create' do
     it "should create new product " do 
       post :create ,:product => iphone4.attributes
-      response.should be_success
+      response.should be_redirect
       flash[:notice].should == "新增商品成功!"
     end
   end
