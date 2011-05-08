771078e82bf8649109666ab9df90e715ebdbc94f
saberma <mahb45@gmail.com>
2011-05-08 12:08:53 +0800

    add custom collection integrate test.

--- spec/requests/custom_collections_spec.rb
+++ spec/requests/custom_collections_spec.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe "SmartCollections" do
-  describe "GET /admin/smart_collections" do
+describe "CustomCollections" do
+  describe "GET /admin/custom_collections" do
 
     let(:user_admin) { Factory :user_admin }
 
@@ -13,24 +13,24 @@ describe "SmartCollections" do
       click_on 'log in'
 
       visit custom_collections_path
-      click_on '新增智能集合'
-      fill_in 'smart_collection[title]', with: 'high price'
+      click_on '新增自定义集合'
+      fill_in 'custom_collection[title]', with: 'high price'
       click_on '保存'
       page.should have_content('high price')
-      SmartCollection.all.size.should eql 1
+      CustomCollection.all.size.should eql 1
 
       click_on '修改'
-      fill_in 'smart_collection[title]', with: 'low price'
+      fill_in 'custom_collection[title]', with: 'low price'
       click_on '保存'
       page.should have_content('low price')
 
-      select '隐藏', :from => 'smart_collection_published'
+      select '隐藏', :from => 'custom_collection_published'
       find('#flashnotice').should have_content('修改成功!')
 
       page.execute_script("window.confirm = function(msg) { return true; }")
       find('.del').click
       page.should_not have_content('high price')
-    end
 
+    end
   end
 end
