f19d0440ac19328804adb9b7cb02daa4c20a69f2
saberma <mahb45@gmail.com>
2011-05-09 11:30:24 +0800

    automate destroy product type and vendor.

--- spec/models/product_observer_spec.rb
+++ spec/models/product_observer_spec.rb
@@ -7,6 +7,8 @@ describe ProductObserver do
 
   let(:iphone4) { Factory :iphone4, shop: shop, product_type: '智能手机', vendor: 'Apple' }
 
+  let(:psp) { Factory :psp, shop: shop, product_type: '游戏机', vendor: 'Sony' }
+
   context '(brand new product type)' do
     it "should be insert" do
       expect do
@@ -17,6 +19,7 @@ describe ProductObserver do
     end
   end
 
+  #类型会被自动删除
   context '(exists product type)' do
     it "should be ignore" do
       iphone4
@@ -25,6 +28,29 @@ describe ProductObserver do
           iphone4.clone.save
         end.to_not change(ShopProductType, :count)
       end.to_not change(ShopProductVendor, :count)
+    end
+
+    #取消唯一关联的类型时，类型会被删除
+    it "should be destroy" do
+      iphone4
+      psp
+      expect do
+        expect do
+          iphone4.update_attributes product_type: '游戏机', vendor: 'Sony'
+        end.to change(ShopProductType, :count).by(-1)
+      end.to change(ShopProductVendor, :count).by(-1)
+    end
+
+    #删除商品或编辑商品时取消唯一关联的类型时，类型会被删除
+    context '(destroy product)' do
+      it "should be destroy" do
+        iphone4
+        expect do
+          expect do
+            iphone4.destroy
+          end.to change(ShopProductType, :count).by(-1)
+        end.to change(ShopProductVendor, :count).by(-1)
+      end
     end
   end
 end
