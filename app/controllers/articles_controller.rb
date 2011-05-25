2ed4eb1fdc513eed2e0e0bfdafc4dd7e3539c214
liwh <liwh87@gmail.com>
2011-05-25 16:04:21 +0800

    add intergration test: blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   def create
     article.user = current_user
     if article.save
-      flash.now[:notice] = I18n.t("flash.actions.#{action_name}.notice")
+      flash[:notice] = I18n.t("flash.actions.#{action_name}.notice")
       redirect_to blog_article_path(blog,article)
     end
   end
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def update
     article.user = current_user
     if article.save
-      flash.now[:notice] = I18n.t("flash.actions.#{action_name}.notice")
+      flash[:notice] = I18n.t("flash.actions.#{action_name}.notice")
       respond_to do |format|
         format.html { redirect_to blog_article_path(blog,article) }
         format.js { render :template => "shared/msg" }
--- db/migrate/20110511132306_create_tags.rb
+++ db/migrate/20110511132306_create_tags.rb
@@ -15,6 +15,7 @@ class CreateTags < ActiveRecord::Migration
       t.references :tag    , comment: '所属标签', null: false
     end
 
+
     add_index :tags         , :shop_id
     add_index :products_tags, :product_id
     add_index :products_tags, :tag_id
