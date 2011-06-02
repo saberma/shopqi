feb4dd62ac4db991f502945fc6725ef781abee60
saberma <mahb45@gmail.com>
2011-06-02 22:58:40 +0800

    extend liquid date filter.

--- config/initializers/liquid.rb
+++ config/initializers/liquid.rb
@@ -3,3 +3,19 @@
 [BaseFilter, UrlFilter, TagFilter].each do |filter|
   Liquid::Template.register_filter filter
 end
+
+module Liquid
+
+    module StandardFilters
+
+      # 修正: 原生date filter不支持'now'参数???
+      def date_with_string(input, format)
+        input = Time.now if input == 'now'
+        date_without_string(input, format)
+      end
+
+      alias_method_chain :date, :string
+
+    end
+
+end
