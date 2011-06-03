d4719dec33d096625ff1b723472084b51190aba0
saberma <mahb45@gmail.com>
2011-06-03 18:08:38 +0800

    pass test.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,13 +5,16 @@ describe ProductsController do
   include Devise::TestHelpers
   
   let(:iphone4) {Factory.build(:iphone4)}
+
   before :each do 
     sign_in(Factory(:user_liwh))
   end
 
   context '#create' do
     it "should create new product " do 
-      post :create ,:product => iphone4.attributes
+      attrs = iphone4.attributes
+      attrs.delete('shop_id')
+      post :create ,:product => attrs
       response.should be_redirect
       flash[:notice].should == "新增商品成功!"
     end
