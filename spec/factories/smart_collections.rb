5beef20dd9af8ea5303d1382f231635bb3fa55f4
saberma <mahb45@gmail.com>
2011-06-05 10:10:51 +0800

    pass all integration test.

--- spec/factories/smart_collections.rb
+++ spec/factories/smart_collections.rb
@@ -8,12 +8,12 @@ end
 
 Factory.define :smart_collection_low_price, parent: :smart_collection do |f|
   f.rules_attributes [
-    {column: 'price', relation: 'less_than', condition: 100}
+    {column: 'variants_price', relation: 'less_than', condition: 100}
   ]
 end
 
 Factory.define :smart_collection_high_price, parent: :smart_collection do |f|
   f.rules_attributes [
-    {column: 'price', relation: 'greater_than', condition: 1000}
+    {column: 'variants_price', relation: 'greater_than', condition: 1000}
   ]
 end
