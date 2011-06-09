6eb93f499b3a835aa0632ca13c60fbb4b1651065
saberma <mahb45@gmail.com>
2011-06-09 21:44:27 +0800

    post order.

--- spec/controllers/shop/order_controller_spec.rb
+++ spec/controllers/shop/order_controller_spec.rb
@@ -9,86 +9,56 @@ describe Shop::OrderController do
 
   let(:variant) { iphone4.variants.first }
 
-  let(:cart) { Factory :cart, shop: shop, cart_hash: "#{variant.id}|1" }
+  let(:cart) { Factory :cart, shop: shop, cart_hash: %Q({"#{variant.id}":1}) }
 
   let(:order) do
     o = Factory.build :order, shop: shop
-    o.products.build product_variant: variant, price: variant.price, quantity: 1
+    o.variants.build product_variant: variant, price: variant.price, quantity: 1
     o.save
+    o
   end
 
-  context '(without products)' do
 
-    context '#address' do
+  let(:billing_address_attributes) { {name: 'ma', province: 'guandong', city: 'shenzhen', district: 'nanshan', address1: '311', phone: '13912345678' } }
 
-      it 'should be error' do
-        get :address, shop_id: shop.id, cart_uuid: cart.uuid
-        assigns['_resources'].should be_nil
-      end
+  context '#address' do
 
+    it 'should be show' do
+      get :address, shop_id: shop.id, cart_uuid: cart.uuid
+      response.should be_success
     end
 
-    context '#create' do
-
-      it 'should be error' do
+    it 'should copy the billding address' do
+      expect do
         post :create, shop_id: shop.id, cart_uuid: cart.uuid, billing_is_shipping: true, order: {
           email: 'mahb45@gmail.com',
+          billing_address_attributes: billing_address_attributes
         }
-        assigns['_resources'].should be_nil
-      end
-
+        order = assigns['_resources']['order']
+        order.shipping_address.name.should eql order.billing_address.name
+        order.shipping_address.address1.should eql order.billing_address.address1
+        order.variants.should_not be_empty
+      end.should change(Order, :count).by(1)
     end
 
   end
 
-  context '(with products)' do
+  context '#update' do
 
-    let(:billing_address_attributes) { {name: 'ma', province: 'guandong', city: 'shenzhen', district: 'nanshan', address1: '311', phone: '13912345678' } }
-
-    before :each do
-      @session = mock('session')
-      @session.stub!(:[], 'cart').and_return("#{variant.id}|1")
-      controller.stub!(:session).and_return(@session)
+    it 'should be pay' do
+      get :pay, shop_id: shop.id, uuid: order.uuid
+      response.should be_success
     end
 
-    context '#address' do
-
-      before :each do
-        get :address, shop_id: shop.id #初始化cookie
-      end
-
-      it 'should copy the billding address' do
-        expect do
-          post :create, shop_id: shop.id, billing_is_shipping: true, order: {
-            email: 'mahb45@gmail.com',
-            billing_address_attributes: billing_address_attributes
-          }
-          order = assigns['_resources']['order']
-          order.shipping_address.name.should eql order.billing_address.name
-          order.shipping_address.address1.should eql order.billing_address.address1
-        end.should change(Order, :count).by(1)
-      end
-
-    end
-
-    context '#update' do
-
-      it 'should be pay' do
-        expect do
-          @session = mock('session')
-          @session.should_receive(:[]).with(:order_params).at_least(:once).and_return(order_attrs)
-          @session.should_receive(:[]).with(:order_step).at_least(:once).and_return('pay')
-          @session.should_receive(:[]=).at_least(:once)
-          controller.stub!(:session).and_return(@session)
-          get :pay, shop_id: shop.id, uuid: order.uuid
-          post :create, shop_id: shop.id, uuid: order.uuid, order: { shipping_rate: '1', gateway: '1' }
-          order = assigns['_resources']['order']
-          order.errors.should be_empty
-          order.shipping_rate.should eql '1'
-          order.gateway.should eql '1'
-        end.should_not change(Order, :count)
-      end
-
+    it 'should show product variants' do
+      order
+      expect do
+        post :commit, shop_id: shop.id, uuid: order.uuid, order: { shipping_rate: '1', gateway: '1' }
+        order = assigns['_resources']['order']
+        order.errors.should be_empty
+        order.shipping_rate.should eql '1'
+        order.gateway.should eql '1'
+      end.should_not change(Order, :count)
     end
 
   end
