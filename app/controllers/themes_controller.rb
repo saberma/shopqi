1c6f7e90f5a82a61a6a88a9b924c93cc1a21d16e
saberma <mahb45@gmail.com>
2011-06-30 22:22:40 +0800

    add theme current page.

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,11 @@
+#encoding: utf-8
 class ThemesController < ApplicationController
+  prepend_before_filter :authenticate_user!
+  layout 'admin'
+
+  expose(:shop) { current_user.shop }
+  expose(:theme) { shop.theme }
+
+  def current
+  end
 end
