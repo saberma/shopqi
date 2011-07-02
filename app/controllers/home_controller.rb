5a27331f1fba760542102026c05858dd59ff3de0
liwh <liwh87@gmail.com>
2011-07-02 19:25:46 +0800

    move the lookup controller feature to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,25 @@
 #encoding: utf-8
 class HomeController < ApplicationController
   prepend_before_filter :authenticate_user! ,:only => [:dashboard]
+  layout 'admin', only: [:dashboard,:query]
+
+  expose(:shop) { current_user.shop }
+
+  expose(:results) do
+    if params[:q].blank?
+      nil
+    else
+      ThinkingSphinx.search params[:q], classes: [Product, Article, Page,Blog], with: { shop_id: shop.id }
+    end
+  end
+
   def index
   end
 
   # 网店管理首页
   def dashboard
-    render :layout => 'admin'
+  end
+
+  def query
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def url_for_lookup(clazz,record, options = {})
+    if clazz == Article
+      polymorphic_url([record.blog,record], options)
+    else
+      polymorphic_url(record, options)
+    end
+  end
 end
