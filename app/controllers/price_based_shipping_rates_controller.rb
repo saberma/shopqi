2c6ed90f3a98fddf4f6a3af8a4fe61888c2bd3fd
liwh <liwh87@gmail.com>
2011-07-10 00:53:39 +0800

    add price based edit action

--- app/controllers/price_based_shipping_rates_controller.rb
+++ app/controllers/price_based_shipping_rates_controller.rb
@@ -13,4 +13,9 @@ class PriceBasedShippingRatesController < ApplicationController
     price_based_shipping_rate.destroy
     flash.now[:notice] = notice_msg
   end
+
+  def update
+    price_based_shipping_rate.save
+    redirect_to edit_price_based_shipping_rate_path(price_based_shipping_rate),notice: notice_msg
+  end
 end
--- app/helpers/price_based_shipping_rates_helper.rb
+++ app/helpers/price_based_shipping_rates_helper.rb
@@ -1,2 +1,5 @@
 module PriceBasedShippingRatesHelper
+  def attr_for_display(obj)
+     obj ? "display:none" : ""
+  end
 end
