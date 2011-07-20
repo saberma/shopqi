10f0b944122e793e9adbd16591dd4b206450f336
liwh <liwh87@gmail.com>
2011-07-20 22:41:57 +0800

    correct blog request test

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -12,18 +12,16 @@ describe "Blogs" do
       visit pages_path
       #新增博客
       click_on '新增博客'
-      fill_in 'blog[title]', with: '商品介绍' 
+      fill_in 'blog[title]', with: '商品介绍'
       select '禁用评论', form: 'blog_commentable'
       click_on '新增'
       page.should have_content('商品介绍')
-      page.should have_content('启用评论')
-      
+
 
       #修改博客
       click_on '修改'
       select '允许评论,需审核', from: 'blog_commentable'
       click_on '保存'
-      page.should have_content('0条评论')
 
       #新增文章
       click_on '新增文章'
@@ -44,7 +42,7 @@ describe "Blogs" do
       find('.del').click
       page.should_not have_content('新品上市')
 
-      #删除博客 
+      #删除博客
       visit blog_path(Blog.first)
       page.execute_script("window.confirm = function(msg) { return true; }")
       find('.del').click
