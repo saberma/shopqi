ac644bb59fddf1a7dda62892236cc05f73bbb2ed
liwh <liwh87@gmail.com>
2011-07-22 01:03:50 +0800

    add gravtastic for users icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class User < ActiveRecord::Base
-
+  include Gravtastic
+  gravtastic
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
