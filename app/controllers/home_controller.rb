fabfa9dbcebdf53f8495c9f6b035ffd23bf979a2
saberma <mahb45@gmail.com>
2011-08-13 11:21:48 +0800

    fixed: skus are not correct in dashboard, issues/141 .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,7 +31,7 @@ class HomeController < ApplicationController
     last_week = shop.orders.between(last_week_begin,last_week_end)
     total = shop.orders.all
     total_product = shop.products.all
-    skus_size = shop.variants.where("sku != ''").size
+    skus_size = shop.variants.size
     {
       today: { price: today.map(&:total_price).inject(0,:+),size: today.size,des: '今天'},
       yesterday: { price: yesterday.map(&:total_price).inject(0,:+),size: yesterday.size,des: '昨天'},
