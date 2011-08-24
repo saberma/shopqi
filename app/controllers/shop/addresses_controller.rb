1291c820f88813d53074371a5204ff883adeaf29
liwh <liwh87@gmail.com>
2011-08-24 18:25:42 +0800

    add default_address column to customer address

--- app/controllers/shop/addresses_controller.rb
+++ app/controllers/shop/addresses_controller.rb
@@ -1,4 +1,6 @@
 class Shop::AddressesController < ApplicationController
   prepend_before_filter :authenticate_customer!
   layout 'shop/admin'
+
+  expose(:addresses){ current_customer.addresses }
 end
