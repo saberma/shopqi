781f0a09faf1aec8bbd1c9e478016e3b277aee25
saberma <mahb45@gmail.com>
2011-10-30 15:59:42 +0800

    close #215. first register do not need the verify code.

--- app/helpers/shopqi/home_helper.rb
+++ app/helpers/shopqi/home_helper.rb
@@ -1,2 +1,11 @@
 module Shopqi::HomeHelper
+
+  def is_registered? # 是否已经注册过商店
+    Rails.cache.read(registered_cache_key)
+  end
+
+  def registered_cache_key
+    "registered_#{request.remote_ip}"
+  end
+
 end
