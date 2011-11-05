c7c690b72be56bf82df3ace5e496368a776c55eb
liwh <liwh87@gmail.com>
2011-11-05 22:47:16 +0800

    change api_client generate method

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -4,9 +4,9 @@ class ApiClient < ActiveRecord::Base
 
   before_create do
     #TODO :修改密钥，key生成方式
-    self.password      = UUID.generate(:compact)
-    self.shared_secret = UUID.generate(:compact)
-    self.api_key       = UUID.generate(:compact)
+    self.password      = ActiveSupport::SecureRandom.hex(32)
+    self.shared_secret = ActiveSupport::SecureRandom.hex(32)
+    self.api_key       = ActiveSupport::SecureRandom.hex(32)
     self.title         = shop.api_clients.blank? ?  "tool - 1" : shop.api_clients.last.title.succ
   end
 end
