b1963fab8281a03c84737e96bddb4c1a54b141c0
liwh <liwh87@gmail.com>
2011-11-05 23:38:53 +0800

    fix deprecated and add resque clear task

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -4,9 +4,9 @@ class ApiClient < ActiveRecord::Base
 
   before_create do
     #TODO :修改密钥，key生成方式
-    self.password      = ActiveSupport::SecureRandom.hex(32)
-    self.shared_secret = ActiveSupport::SecureRandom.hex(32)
-    self.api_key       = ActiveSupport::SecureRandom.hex(32)
+    self.password      = SecureRandom.hex(32)
+    self.shared_secret = SecureRandom.hex(32)
+    self.api_key       = SecureRandom.hex(32)
     self.title         = shop.api_clients.blank? ?  "tool - 1" : shop.api_clients.last.title.succ
   end
 end
