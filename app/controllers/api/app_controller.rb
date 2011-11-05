d313e1fac4e13fb6062ec4f3947b7e090a850fee
liwh <liwh87@gmail.com>
2011-11-05 22:36:31 +0800

    perfect the api controller filter

--- app/controllers/api/app_controller.rb
+++ app/controllers/api/app_controller.rb
@@ -5,16 +5,26 @@ class Api::AppController < ActionController::Base # API接口
 
   protected
   def authenticate
-    myshopqi = Shop.at( request.host)
-    authenticate_or_request_with_http_basic do |username, password|
-      myshopqi.api_clients.exists?(api_key: username,password: password)
+    unless session[:shop]
+      myshopqi = Shop.at( request.host)
+      authenticate_or_request_with_http_basic do |username, password|
+        if myshopqi.api_clients.exists?(api_key: username,password: password)
+          session[:shop] ||=  myshopqi.as_json(only: [:deadline, :created_at, :updated_at, :name])['shop']
+        else
+          render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'}
+        end
+      end
     end
   end
 
   def login_or_oauth_required
-    token = OAuth2::Provider.access_token(nil, [], request)
-    unless token.valid?
-      render json: {error: 'No soup for you!'}
+    unless session[:shop]
+      token = OAuth2::Provider.access_token(nil, [], request)
+      unless token.valid?
+        render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'}
+      else
+        session[:shop] ||= token.owner.as_json(only: [:deadline, :created_at, :updated_at, :name])['shop']
+      end
     end
   end
 end
