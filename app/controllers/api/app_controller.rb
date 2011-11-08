bb6851cc8fe7b069973bfc4b5bd32403de47649c
liwh <liwh87@gmail.com>
2011-11-08 14:22:57 +0800

    add crud action to api controllers

--- app/controllers/api/app_controller.rb
+++ app/controllers/api/app_controller.rb
@@ -12,6 +12,37 @@ class Api::AppController < ActionController::Base # API接口
     end
   end
 
+  def show
+    respond_with(@object) do |format|
+      format.json { render :json => @object.to_json(object_serialization_options) }
+    end
+  end
+
+  def update
+    if @object.update_attributes(params[object_name])
+      respond_with(@object) do |format|
+        format.json { render :json => @object.to_json(object_serialization_options) }
+      end
+    else
+      respond_with(@object.errors,status: 422)
+    end
+  end
+
+  def create
+    if @object.save
+      respond_with(@object) do |format|
+        format.json { render :json => @object.to_json(object_serialization_options) }
+      end
+    else
+      respond_with(@object.errors,status: 422)
+    end
+  end
+
+  def destroy
+    @object.destroy
+    render nothing: true
+  end
+
   protected
   def model_class
     controller_name.classify.constantize
@@ -109,7 +140,7 @@ class Api::AppController < ActionController::Base # API接口
         if myshopqi.api_clients.exists?(api_key: username,password: password)
           session[:shop] ||=  myshopqi.as_json(only: [:deadline, :created_at, :updated_at, :name])['shop']
         else
-          render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'}
+          render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'} , status: 401
         end
       end
     end
@@ -119,7 +150,7 @@ class Api::AppController < ActionController::Base # API接口
     unless session[:shop]
       token = OAuth2::Provider.access_token(nil, [], request)
       unless token.valid?
-        render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'}
+        render json: {error: '[API] Invalid API key or permission token (unrecognized login or wrong password)'} , status: 401
       else
         session[:shop] ||= token.owner.as_json(only: [:deadline, :created_at, :updated_at, :name])['shop']
       end
