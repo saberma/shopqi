0d719e8d1a2184445f992b977f9f09f3a9e510da
saberma <mahb45@gmail.com>
2011-11-10 17:55:05 +0800

    add customer login form, and layout tag. #249.

--- spec/liquids/form_spec.rb
+++ spec/liquids/form_spec.rb
@@ -5,16 +5,35 @@ describe Form do
 
   let(:shop) { Factory(:user_liwh).shop }
 
-  let(:blog) { shop.blogs.where(handle: 'news').first }
+  describe Article do
 
-  let(:article) { Article.create title: '文章', shop_id: shop.id, blog_id: blog.id , body_html: '新文章。。。'}
+    let(:blog) { shop.blogs.where(handle: 'news').first }
 
-  let(:article_drop) { ArticleDrop.new(article) }
+    let(:article) { Article.create title: '文章', shop_id: shop.id, blog_id: blog.id , body_html: '新文章。。。'}
 
-  it 'should get the right form tag' do
-    variant = "{% form article %}{% endform %}"
-    assign = { 'article' => article_drop }
-    Liquid::Template.parse(variant).render(assign).should eql "<form id=\"article-#{article.id}-comment-form\" class=\"comment-form\" method=\"post\" action=\"/blogs/#{article.blog.handle}/#{article.id}/comments\">\n\n</form>"
+    let(:article_drop) { ArticleDrop.new(article) }
+
+    it 'should get the right form tag' do
+      variant = "{% form article %}{% endform %}"
+      assign = { 'article' => article_drop }
+      Liquid::Template.parse(variant).render(assign).should eql "<form id=\"article-#{article.id}-comment-form\" class=\"comment-form\" method=\"post\" action=\"/blogs/#{article.blog.handle}/#{article.id}/comments\">\n\n</form>"
+    end
+
+  end
+
+  describe Customer do
+
+    let(:customer) { Factory :customer_saberma, shop: shop }
+
+    let(:customer_drop) { CustomerDrop.new customer }
+
+    it 'should show login from', focus: true do
+      variant = "{% form 'customer_login' %}{% endform %}"
+      assign = { 'customer' => customer_drop }
+      result = "<form id=\"customer_login\" method=\"post\" action=\"/accounts/login\">\n\n</form>"
+      Liquid::Template.parse(variant).render(assign).should eql result
+    end
+
   end
 
 end
