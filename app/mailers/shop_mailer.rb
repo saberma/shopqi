b87407b84c70d122545c8a78efacc64ad91555ef
liwh <liwh87@gmail.com>
2011-11-15 15:42:40 +0800

    fixed #278

--- app/mailers/shop_mailer.rb
+++ app/mailers/shop_mailer.rb
@@ -9,7 +9,7 @@ class ShopMailer < ActionMailer::Base
     liquid_hash = liquid_drop.as_json
     body        = email_template.include_html ? email_template.body_html : email_template.body #判断用text/plain模版还是html模版
     type        = email_template.include_html ? 'text/html' : 'text/plain'
-    mail(to: email,from: Shop.find(shop_id).email, body: Liquid::Template.parse(body).render(liquid_hash), subject: Liquid::Template.parse(email_template.title).render(liquid_hash),content_type: type)
+    mail(to: email,from: Shop.find(shop_id).email, body: Liquid::Template.parse(body).render(liquid_hash.merge('shop' => ShopDrop.new(order.shop))), subject: Liquid::Template.parse(email_template.title).render(liquid_hash),content_type: type)
   end
 
 end
