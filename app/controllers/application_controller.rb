be4fe681c13c69c1b69fa0fe8f070da1b6850bd3
liwh <liwh87@gmail.com>
2011-11-15 20:03:42 +0800

    add reset password for liquid shop #258 ,and fix #279

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # encoding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_host_for_mailer
+
+  protected
+  def set_host_for_mailer #用于设置发送邮件时的host,放置这里，主要是devise的控制器继承这个控制器
+    ActionMailer::Base.default_url_options[:host] = "#{request.host}#{request.port_string}"
+  end
 end
