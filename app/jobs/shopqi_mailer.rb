2752b42eee99c8881f8ec7bda0033275f1ad679a
liwh <liwh87@gmail.com>
2011-11-15 15:04:05 +0800

    refactory the shop email and add spec test ,close #257

--- app/jobs/shopqi_mailer.rb
+++ app/jobs/shopqi_mailer.rb
@@ -1,10 +1,7 @@
 module ShopqiMailer
   @queue = "shopqi_mailer"
 
-  def self.perform(email,order_id,email_template_id,shop_id)
-    order = Order.find(order_id)
-    liquid_drop = OrderDrop.new order
-    email_template = Email.find(email_template_id)
-    ShopMailer.notify_email(email,liquid_drop,email_template,shop_id).deliver
+  def self.perform(email,order_id,mail_type,shop_id)
+    ShopMailer.notify_email(email,order_id,mail_type,shop_id).deliver
   end
 end
--- app/mailers/shop_mailer.rb
+++ app/mailers/shop_mailer.rb
@@ -2,7 +2,10 @@
 #用于发送各网店的邮件,例如：订单提醒等
 class ShopMailer < ActionMailer::Base
 
-  def notify_email(email,liquid_drop,email_template,shop_id)
+  def notify_email(email,order_id,mail_type,shop_id)
+    order = Order.find(order_id)
+    liquid_drop = OrderDrop.new order
+    email_template =  order.shop.emails.find_by_mail_type(mail_type)
     liquid_hash = liquid_drop.as_json
     body        = email_template.include_html ? email_template.body_html : email_template.body #判断用text/plain模版还是html模版
     type        = email_template.include_html ? 'text/html' : 'text/plain'
