4bae3b57b7c5d7af4ea7c1a20c3c54dbf9f8ccb3
saberma <mahb45@gmail.com>
2011-11-20 16:08:35 +0800

    fixed #289, theme settings upload photo fail.

--- spec/controllers/admin/assets_controller_spec.rb
+++ spec/controllers/admin/assets_controller_spec.rb
@@ -1,16 +1,21 @@
-=begin
 require 'spec_helper'
 
-describe AssetsController do
+describe Admin::AssetsController do
   include Devise::TestHelpers
+
+  let(:theme_dark) { Factory :theme_woodland_dark }
 
   let(:user) { Factory(:user) }
 
-  let(:shop) { user.shop }
+  let(:shop) do
+    model = user.shop
+    model.themes.install theme_dark
+    model
+  end
 
   let(:theme) { shop.theme }
 
-  let(:path) { path = File.join Rails.root, 'public', 's', 'files', Rails.env, shop.id.to_s, 'theme'}
+  let(:path) { File.join Rails.root, 'data', 'shops', Rails.env, shop.id.to_s, 'themes', theme.id.to_s}
 
   before :each do
     request.host = "#{shop.primary_domain.host}"
@@ -18,10 +23,25 @@ describe AssetsController do
   end
 
   it 'should be create' do
-    post :create, key: 'layout/test.liquid', source_key: 'layout/theme.liquid'
+    post :create, key: 'layout/test.liquid', source_key: 'layout/theme.liquid', theme_id: theme.id
     file_path = File.join path, 'layout/test.liquid'
     File.exist?(file_path).should be_true
   end
 
+  context '#upload' do # 上传附件
+
+    let(:file_path) { Rails.root.join('spec', 'factories', 'data', 'themes') }
+
+    describe 'ie', focus: true do # 支持ie浏览器上传
+
+      it 'should be success' do
+        post :upload, key: 'assets/product.jpg', theme_id: theme.id, id: 0, qqfile: Rack::Test::UploadedFile.new(File.join(file_path, 'product.jpg'))
+        response.should be_success
+        response.content_type.should eql 'text/html' # context_type不为'text/html'时，ie会将json作为文件下载
+      end
+
+    end
+
+  end
+
 end
-=end
