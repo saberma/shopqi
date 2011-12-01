f3eebb4ba2b2f4b405d84995c7a40d8000d1453c
saberma <mahb45@gmail.com>
2011-12-01 21:58:22 +0800

    fixed #330, soft link should use real path.

--- spec/models/shop_theme_spec.rb
+++ spec/models/shop_theme_spec.rb
@@ -133,6 +133,26 @@ describe ShopTheme do
 
   end
 
+  describe 'link path' do
+
+    # capistrano部署时会将current 链接到 release_path 目录，但FileUtils.ln_s不能指向release_path，旧有release_path会随时被删除
+    it 'should use source path, instead of linked path' do
+      data_path = "/tmp/deploy/#{DateTime.now.to_i}/shared/data" # shared/data
+      current_data_path = Rails.root.join 'data_test' # release_path/data
+      shop.stub!(:path) { current_data_path }
+      FileUtils.mkdir_p data_path
+      FileUtils.ln_sf data_path, current_data_path
+      theme = ShopTheme.create theme_id: theme_dark.id, role: theme_dark.role, shop: shop
+      FileUtils.rm_rf "/tmp/deploy"
+      FileUtils.rm_f current_data_path
+      file = File.join theme.public_path, 'assets'
+      target = File.readlink file
+      target.should_not eql "#{current_data_path}/themes/1/assets" # 不能指向 release_path
+      target.should eql "#{data_path}/themes/#{theme.id}/assets"
+    end
+
+  end
+
   def data
     {use_logo_image:"0", logo_color:"#ffffff", use_logobackground_image:"1", use_feature_image:"1", use_bannerbackground_image:"1", use_bannerurl_image:"danielweinand.com", use_header_image:"1", header_color:"#ffffff", body_background:"1", background_color:"#000000", text_color:"#000000", heading_color:"#000000", link_color:"#970000", linkhover_color:"#680000", mainnav_color:"#ffffff", mainnavhover_color:"#ffffff", mainnavline_color:"#ffffff", subnav_color:"#937c72", subnavhover_color:"#f1bda9", footertext_color:"#d5c4bf", footerlink_color:"#915c4f", footerline_color:"#674a43", checkoutheader_color:"#767676", header_font:"'Courier New', Courier, monospace", regular_font:"Helvetica, Arial, sans-serif", cart_graphic:"1", border_color:"#b7b7c2", border_grunge:"1", productborder_color:"#f2f2f2", productborderhover_color:"#ffffff", use_footer_image:"1", bgfooter_color:"#000000", customer_layout:"theme"}
   end
