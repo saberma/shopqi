efef59ea0199949d93969bab6f5d51b547442574
saberma <mahb45@gmail.com>
2011-12-20 22:49:48 +0800

    fixed #354, show the correct page offset.

--- app/liquids/paginate.rb
+++ app/liquids/paginate.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class Paginate < Liquid::Block
   Syntax = /(#{Liquid::VariableSignature}+)\s+by\s+(\d+)/
 
@@ -24,19 +25,19 @@ class Paginate < Liquid::Block
       context['paginate'] = {
         'page_size'  => @size,
         'current_page'  => current_page,
-        'current_offset'  => current_page * @size,
+        'current_offset'  => (current_page - 1) * @size,
         'pages'  => pages,
         'items'  => items,
       }
       if current_page > 1
-        context['paginate']['previous'] = { 'url' => "?page=#{current_page - 1}", 'title' => 'Previous' }
+        context['paginate']['previous'] = { 'url' => "?page=#{current_page - 1}", 'title' => '上一页' }
       end
       query_param = context['q'] ? "&q=#{context['q']}" : ""
       context['paginate']['parts'] = (1..pages).map do |page|
         { 'url' => "?page=#{page}#{query_param}", 'title' => page, 'is_link' => (page != current_page) }
       end
       if current_page < pages
-        context['paginate']['next'] = { 'url' => "?page=#{current_page + 1}#{query_param}", 'title' => 'Next' }
+        context['paginate']['next'] = { 'url' => "?page=#{current_page + 1}#{query_param}", 'title' => '下一页' }
       end
 
       super
--- spec/liquids/paginate_spec.rb
+++ spec/liquids/paginate_spec.rb
@@ -18,6 +18,13 @@ describe Paginate do
     Liquid::Template.parse(variant).render(assign).should eql "1,1"
   end
 
+  it 'should get current_offset' do # 已经偏移的位数(当前页的起始序号-1)
+    iphone4
+    variant = "{% paginate collection.products by 1 %}{{paginate.current_offset}}{% endpaginate %}"
+    assign = { 'collection' => collection_drop, 'current_page' => 1 }
+    Liquid::Template.parse(variant).render(assign).should eql '0'
+  end
+
   it 'should remain the methods' do
     variant = "{% paginate collection.products by 2 %}{{collection.title}}{% endpaginate %}"
     assign = { 'collection' => collection_drop, 'current_page' => 1 }
