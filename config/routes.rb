ae9c7d430074d3a7d6b99ca5b1e3557a84082eb2
saberma <mahb45@gmail.com>
2011-12-26 10:15:32 +0800

    add /products route alias for /collections.

--- config/routes.rb
+++ config/routes.rb
@@ -146,6 +146,7 @@ Shopqi::Application.routes.draw do
       get '/products/:handle'                               , to: 'products#show'       , as: :product_show
       get '/collections/:collection_handle/products/:handle', to: 'products#show'       , as: :collection_product_show
       get '/collections'                                    , to: 'collections#index'
+      get '/products'                                       , to: 'collections#index'
       get '/collections/:handle'                            , to: 'collections#show'
       get '/collections/:handle/products'                   , to: 'collections#show'
       get '/pages/:handle'                                  , to: 'pages#show'
