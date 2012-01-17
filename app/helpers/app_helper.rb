806b2fec860763efba9816c32b6def3feef73af8
saberma <mahb45@gmail.com>
2012-01-17 10:15:05 +0800

    fixed shopqi home page 400 phone css height error(ie6). #391

---
+++ app/helpers/app_helper.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+module AppHelper
+
+  def conditional_html( lang = "en", &block ) # http://j.mp/xx2QPw
+    haml_concat Haml::Util::html_safe <<-"HTML".gsub( /^\s+/, '' )
+      <!--[if lt IE 7 ]>              <html lang="#{lang}" class="no-js ie6"> <![endif]-->
+      <!--[if IE 7 ]>                 <html lang="#{lang}" class="no-js ie7"> <![endif]-->
+      <!--[if IE 8 ]>                 <html lang="#{lang}" class="no-js ie8"> <![endif]-->
+      <!--[if IE 9 ]>                 <html lang="#{lang}" class="no-js ie9"> <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!--> <html lang="#{lang}" class="no-js"> <!--<![endif]-->      
+    HTML
+    haml_concat capture( &block ) << Haml::Util::html_safe( "\n</html>" ) if block_given?
+  end
+
+end
