9c4ae940b9ea3330c90c114101f6e2f9880d0fdb
saberma <mahb45@gmail.com>
2012-02-01 20:09:41 +0800

    minor change.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ Spork.prefork do
   require 'capybara/rspec'
   require 'capybara/rails'
   Capybara.default_wait_time = 5
+  Capybara.server_boot_timeout = 50
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
