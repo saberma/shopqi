a576378934fad8ef7538aedbaa6a5e18d96d5692
saberma <mahb45@gmail.com>
2012-03-12 11:53:11 +0800

    add shipping integration test. #407

--- spec/requests/shippings_spec.rb
+++ spec/requests/shippings_spec.rb
@@ -2,22 +2,52 @@
 require 'spec_helper'
 require 'shared_stuff'
 
-describe "Shippings" do
+describe "Shippings", js: true do
 
   include_context 'login admin'
 
-  describe "GET /admin/shipping/index" do
+  before :each do
+    visit shippings_path
+  end
 
-    it "works! ", js: true do
-      visit shipping_index_path
-      page.should have_content('中国')
-      find_field('weight_based_shipping_rate[name]').visible?.should be_false
-      find_field('weight_based_shipping_rate[weight_low]').visible?.should be_false
-      find_field('weight_based_shipping_rate[weight_high]').visible?.should be_false
+  describe "GET /admin/shipping" do
 
-      find_field('price_based_shipping_rate[name]').visible?.should be_false
-      find_field('price_based_shipping_rate[min_order_subtotal]').visible?.should be_false
-      find_field('price_based_shipping_rate[max_order_subtotal]').visible?.should be_false
+    describe Shipping do
+
+      it "should be index" do # 显示列表
+        page.should have_content('全国') # 默认记录
+        page.should have_content('普通快递') # 默认记录
+        page.should have_content('0kg - 25kg')
+        page.should have_content('10元')
+      end
+
+      it "should be add" do # 新增
+        click_on '新增目的地'
+        within '#new-region' do
+          select '广东省', form: 'province'
+          select '深圳市', form: 'city'
+          select '南山区', form: 'district'
+          click_on '保存'
+        end
+        has_content?('新增成功!').should be_true
+        within '#custom-shipping' do
+          find(:xpath, './table[2]//th[1]').text.should eql '广东省深圳市南山区'
+        end
+      end
+
+      it "should be destroy" do # 删除
+        within '#custom-shipping' do
+          within :xpath, './table[1]' do
+            page.execute_script("window.confirm = function(msg) { return true; }")
+            find('.del').click
+          end
+          page.should have_no_xpath('./table[1]')
+        end
+      end
+
+    end
+
+    describe "WeightBasedShippingRate" do
     end
 
   end
