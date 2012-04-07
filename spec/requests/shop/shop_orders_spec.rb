acc17ae6881477bc117698aa69f3e01820f6309f
saberma <mahb45@gmail.com>
2012-04-07 17:48:55 +0800

    add discount apply integration test. #415

--- spec/requests/shop/shop_orders_spec.rb
+++ spec/requests/shop/shop_orders_spec.rb
@@ -80,4 +80,63 @@ describe "Orders", js: true do
 
   end
 
+  ##### 优惠码 #####
+  describe "Discount" do
+
+    let(:payment) { Factory :payment, shop: shop }
+
+    let(:discount) { shop.discounts.create code: 'COUPON123', value: 10, usage_limit: 20 }
+
+    context 'exist discount' do # 商店有优惠码
+
+      before do
+        payment
+        discount
+      end
+
+      it 'should show the discount form' do # 要显示优惠码输入框
+        visit "/cart"
+        click_on '结算'
+        fill_in 'discount[code]', with: discount.code
+        click_on '提交'
+        find('#discount-detail').should have_content("您正使用优惠码 #{discount.code} 节省了 10 元。")
+        find('#cost').should have_content("¥#{(iphone4_variant.price - discount.value).to_i}")
+        #收货人
+        fill_in 'order[email]', with: 'mahb45@gmail.com'
+        fill_in 'order[shipping_address_attributes][name]', with: '马海波'
+        select '广东省', form: 'order[shipping_address_attributes][province]'
+        select '深圳市', form: 'order[shipping_address_attributes][city]'
+        select '南山区', form: 'order[shipping_address_attributes][district]'
+        fill_in 'order[shipping_address_attributes][address1]', with: '311'
+        fill_in 'order[shipping_address_attributes][phone]', with: '13928458888'
+        choose '普通快递-¥10' #选择配送方式
+        choose '邮局汇款' #选择支付方式
+        click_on '提交订单'
+        page.should have_content("您的订单号为： #1001")
+        visit new_user_session_path # 登录后台查看订单
+        fill_in 'user[email]', with: user_admin.email
+        fill_in 'user[password]', with: user_admin.password
+        click_on '登录'
+        visit orders_path
+        click_on '#1001'
+        within '#discount' do # 显示优惠码和优惠金额
+          find(:xpath, ".//td[2]").should have_content(discount.code)
+          find(:xpath, ".//td[3]").should have_content("-10")
+        end
+        find('#price-summary').should have_content((iphone4_variant.price - discount.value).to_i) # 小计要减掉优惠金额
+      end
+
+    end
+
+    context 'do not exist discount' do # 没有优惠码
+
+      it 'should not show the discount form' do # 不显示优惠码输入框
+        visit "/cart"
+        click_on '结算'
+        page.should_not have_content('如果您有优惠码，请在此处输入')
+      end
+    end
+
+  end
+
 end
