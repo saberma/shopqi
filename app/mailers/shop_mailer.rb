8f8bced886093ca5c80b352b6400494024202f52
saberma <mahb45@gmail.com>
2012-04-11 12:22:03 +0800

    email should use shop name.

--- app/mailers/shop_mailer.rb
+++ app/mailers/shop_mailer.rb
@@ -4,13 +4,14 @@ class ShopMailer < ActionMailer::Base
   default from: Setting.mail_from
 
   def notify_email(email,order_id,mail_type,shop_id)
+    shop = Shop.find(shop_id)
     order = Order.find(order_id)
     liquid_drop = OrderDrop.new order
     email_template =  order.shop.emails.find_by_mail_type(mail_type)
     liquid_hash = liquid_drop.as_json
     body        = email_template.include_html ? email_template.body_html : email_template.body #判断用text/plain模版还是html模版
     type        = email_template.include_html ? 'text/html' : 'text/plain'
-    mail(to: email,from: Shop.find(shop_id).email, body: Liquid::Template.parse(body).render(liquid_hash.merge('shop' => ShopDrop.new(order.shop))), subject: Liquid::Template.parse(email_template.title).render(liquid_hash),content_type: type)
+    mail(to: email,from: "#{shop.name} <#{shop.email}>", body: Liquid::Template.parse(body).render(liquid_hash.merge('shop' => ShopDrop.new(order.shop))), subject: Liquid::Template.parse(email_template.title).render(liquid_hash),content_type: type)
   end
 
   def contact_us(email, body, name, shop_id) # 顾客在商店提交联系表单时发送
