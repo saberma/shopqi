12c47dbd364ac58250457e7570f8429b36b84138
saberma <mahb45@gmail.com>
2012-06-30 12:37:49 +0800

    close #456, show installed applications rspec test.

--- spec/controllers/admin/home_controller_spec.rb
+++ spec/controllers/admin/home_controller_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe Admin::HomeController do
 
+  let(:user) { Factory(:user) }
+
+  let(:shop) { user.shop }
+
+  before :each do
+    request.host = "#{shop.primary_domain.host}"
+    sign_in(user)
+  end
+
+  context 'installed application' do # 已安装应用
+
+    render_views
+
+    let(:application) { Factory :express_application } # OAuth application
+
+    before do # 商店授权
+      application.access_tokens.create! resource_owner_id: shop.id, expires_in: Doorkeeper.configuration.access_token_expires_in, scopes: 'read_orders'
+    end
+
+    it 'should be list' do # 在后台管理的应用列表中显示
+      get :dashboard
+      response.body.should include application.name
+      response.body.should include "#{application.redirect_uri}_login?shop=#{shop.shopqi_domain}" # 登录地址
+    end
+
+  end
+
 end
--- spec/helpers/oauth2_spec.rb
+++ spec/helpers/oauth2_spec.rb
@@ -9,7 +9,7 @@ describe 'Doorkeeper::OAuth::AuthorizationRequest' do
 
   context '#success_redirect_uri' do
 
-    it 'should get shop params' do
+    it 'should get shop params' do # callback 回传给应用时要带上 shop 参数
       authorize = Doorkeeper::OAuth::AuthorizationRequest.new application, shop, response_type: 'code', redirect_uri: application.redirect_uri
       authorize.authorize
       authorize.success_redirect_uri.should include("&shop=#{shop.shopqi_domain}")
