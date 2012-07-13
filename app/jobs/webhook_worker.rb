12828f296f3633d517c0788c82f62debf7616f08
saberma <mahb45@gmail.com>
2012-07-13 17:55:05 +0800

    fixed httparty resque 411 code error.

--- app/jobs/webhook_worker.rb
+++ app/jobs/webhook_worker.rb
@@ -4,9 +4,9 @@ module WebhookWorker
   @queue = "webhook_worker"
 
   def self.perform(callback_url, options, time = 0)
-    response = HTTParty.post(callback_url, options)
+    response = HTTParty.post(callback_url, options.symbolize_keys) # fixed 411 错误: options 中的 key 一定要是 Symbolize
     if response.code != 200 # 发生错误
-      if time > 0 and time < EXPONENTIAL_BACKOFF.size
+      if time >= 0 and time < EXPONENTIAL_BACKOFF.size
         Resque.enqueue_in(EXPONENTIAL_BACKOFF[time].seconds, WebhookWorker, callback_url, options, time + 1)
       else
         # TODO: 发邮件通知商店管理员或应用网站管理员
--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -1,2 +1,4 @@
 require 'resque'
 require 'resque/server'
+require 'resque_scheduler'
+require 'resque_scheduler/server'
