eb98cf3c21a1b05c54d04baaac347b863ccf0c9b
saberma <mahb45@gmail.com>
2012-07-27 17:36:17 +0800

    fixed: Can't mass-assign protected attributes: resource_id

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,5 +2,5 @@ class Permission < ActiveRecord::Base
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to_active_hash :resource      , class_name: 'KeyValues::Resource'
-  attr_accessible []
+  attr_accessible :resource_id
 end
