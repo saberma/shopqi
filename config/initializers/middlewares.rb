626898e4cd7dcd25d21b4af3139e9bab10abf097
saberma <mahb45@gmail.com>
2012-07-28 11:27:33 +0800

    use exception notifier in production env.

--- config/initializers/middlewares.rb
+++ config/initializers/middlewares.rb
@@ -1,4 +1,6 @@
-Shopqi::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[ShopQi] ",
-  :sender_address => %{"ShopQi Notifier"},
-  :exception_recipients => %W{#{SecretSetting.author.email}}
+if production?
+  Shopqi::Application.config.middleware.use ExceptionNotifier,
+    :email_prefix => "[ShopQi] ",
+    :sender_address => %{"ShopQi Notifier"},
+    :exception_recipients => %W{#{SecretSetting.author.email}}
+end
